69911842983f459396958a2595da75a38a2a2151
Matthias Kostka <matthias@kostkams.de>
2018-03-21 00:54:39 +0100

    React TS added

--- webpack.config.js
+++ webpack.config.js
@@ -1,22 +1,33 @@
 module.exports = {
-    mode: 'development',
-    entry: './src/index.ts',
+    entry: "./src/index.tsx",
     output: {
-        filename: 'bundle.js',
-        path: __dirname
+        filename: "bundle.js",
+        path: __dirname + "/dist"
     },
+
+    // Enable sourcemaps for debugging webpack's output.
+    devtool: "source-map",
+
+    resolve: {
+        // Add '.ts' and '.tsx' as resolvable extensions.
+        extensions: [".ts", ".tsx", ".js", ".json"]
+    },
+
     module: {
         rules: [
-            {
-                test: /\.tsx?$/,
-                loader: 'ts-loader',
-                exclude: /node_modules/
-            }
+            // All files with a '.ts' or '.tsx' extension will be handled by 'awesome-typescript-loader'.
+            { test: /\.tsx?$/, loader: "ts-loader" },
+
+            // All output '.js' files will have any sourcemaps re-processed by 'source-map-loader'.
+            { enforce: "pre", test: /\.js$/, loader: "source-map-loader" }
         ]
     },
-    resolve: {
-        extensions: [".tsx", ".ts", ".js"]
-    },
+
+    // When importing a module whose path matches one of the following, just
+    // assume a corresponding global variable exists and use that instead.
+    // This is important because it allows us to avoid bundling all of our
+    // dependencies, which allows browsers to cache those libraries between builds.
+
     devServer: {
         inline:true,
         port: 1111
